7208c634008a13296401a916d422859c084bc90f
yushusoft <qiuyuchun@163.com>
2023-10-22 19:48:33 +0800

    初始化 Signed-off-by: yushusoft <qiuyuchun@163.com>

---
+++ spring-boot-demo-master/spring-boot-elasticsearch/spring-boot-elasticsearch-7/src/main/java/com/example/dto/User.java
@@ -0,0 +1,17 @@
+package com.example.dto;
+
+import lombok.Data;
+
+/**
+ * <p>
+ * 原生操作的测试对象
+ * </p>
+ *
+ * @author MrWen
+ **/
+@Data
+public class User {
+    private String name;
+    private String sex;
+    private Integer age;
+}
---
+++ spring-boot-demo-master/springboot-01-cache/src/main/java/com/yushu/cache/springboot01cache/controller/DeptController.java
@@ -0,0 +1,21 @@
+package com.yushu.cache.springboot01cache.controller;
+
+
+import com.yushu.cache.springboot01cache.bean.Department;
+import com.yushu.cache.springboot01cache.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeptController {
+
+    @Autowired
+    DeptService deptService;
+
+    @GetMapping("/dept/{id}")
+    public Department getDept(@PathVariable("id") Integer id){
+        return deptService.getDeptById(id);
+    }
+}
