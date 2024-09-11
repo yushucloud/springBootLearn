73da8b8d3811a9534a196c9caea564477cfb42dd
yushusoft <qiuyuchun@163.com>
2024-09-11 21:29:41 +0800

    项目初始化

---
+++ spring-boot-devtools/src/main/java/com/songguoliang/springboot/SpringBootDevtoolsApplication.java
@@ -0,0 +1,18 @@
+package com.songguoliang.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author sgl
+ * @Date 2018-05-02 14:51
+ */
+@SpringBootApplication
+public class SpringBootDevtoolsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDevtoolsApplication.class, args);
+    }
+
+
+}
